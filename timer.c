8a310bf4b4e95b693fce5cb57a57ce473594774b
John <john@acme.com>
2021-04-29 11:17:37 -0400

    Fix

--- timer.c
+++ timer.c
@@ -111,7 +111,7 @@ void delete_timer_record(int idx)
     int i;
     
     /* fill in the holes */
-    for (i = idx; i < curr_index; i++)
+    for (i = idx; i < curr_index -1; i++)
     {
         if (0 == timer_records[i]) {
             break;
