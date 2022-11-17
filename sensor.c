0494a134098bd5ed44ce8a27aa322a40c269207d
Mirek Zielinski <mirek@parasoft.com>
2022-11-17 08:26:06 -0500

    New func

--- sensor.c
+++ sensor.c
@@ -4,7 +4,7 @@
 #include "mathLib.h"
 
 
-/*
+
 int8_t getSensorValue(uint8_t sensorId, int32_t * value) {
     int32_t * sensors;
     int8_t status;
@@ -18,7 +18,7 @@ int8_t getSensorValue(uint8_t sensorId, int32_t * value) {
     }
     return status;
 }
-*/
+
 
 struct Sensors getSensors(void) {
     //Direct read from memory - to be implemented
