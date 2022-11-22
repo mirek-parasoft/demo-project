a342e347bddd6b64622ad038c6b52a61a64d19eb
Mirek Zielinski <mirek@parasoft.com>
2022-11-22 08:53:18 -0500

    New code

--- sensor.c
+++ sensor.c
@@ -3,14 +3,12 @@
 #include "mathLib.h"
 
 int8_t getSensorValue(uint8_t sensorId, int32_t * value) {
-    int32_t * sensors;
     int8_t status;
-    sensors = getSensors().array; 
 
     if (sensorId > INSTALLED_SENSORS) {
         status = STATUS_ERROR;        
     } else {
-        *value = sensors[sensorId];
+        *value = getSensors().array[sensorId];
         status = STATUS_OK;
     }
     return status;
